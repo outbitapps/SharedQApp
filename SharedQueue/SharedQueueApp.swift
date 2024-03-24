9941b324e9d73a71587358229bd45a8984a4ed98
paytondev <81662879+paytontech@users.noreply.github.com>
2024-03-24 00:20:23 -0400

    Initial Commit

---
+++ SharedQueue/SharedQueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedQueueApp.swift
+//  SharedQueue
+//
+//  Created by Payton Curry on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SharedQueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
